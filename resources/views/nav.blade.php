45d992ebe1a9a376fe91ba6ed1ce3a2c6b0c0fae
Tobias-Be <56866339+Tobias-Be@users.noreply.github.com>
2021-01-15 19:28:18 +0100

    Create nav.blade.php

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">Startseite</a></li>
+        <li><a href="/artikeluebersicht">Artikelübersicht</a></li>
+        <li><a href="/artikeluebersicht/create">Artikel hinzufügen</a></li>
+        <li><a href="/delete">Artikel löschen</a></li>
+    </ul>
+</nav>
