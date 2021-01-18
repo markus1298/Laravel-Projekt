cde0fa1feb9d4af4accdcf9f99dc4d94c4532523
Tobias-Be <tobi.benrath@gmail.com>
2021-01-18 13:38:53 +0100

    nav verändert

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,6 +3,5 @@
         <li><a href="/">Startseite</a></li>
         <li><a href="/artikel">Artikelübersicht</a></li>
         <li><a href="/hinzufügen">Artikel hinzufügen</a></li>
-        <li><a href="/bearbeiten">Artikel bearbeiten</a></li>
     </ul>
 </nav>
